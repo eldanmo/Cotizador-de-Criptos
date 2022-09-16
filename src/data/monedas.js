6f85d0008a0b0567437b5186a5a85c9d3efba740
elDanmo <daniel_mora0407@outlook.com>
2022-09-16 10:53:09 -0500

    Se carga el proyecto terminado

---
+++ src/data/monedas.js
@@ -0,0 +1,10 @@
+const monedas = [
+    {id: 'COP', nombre: 'Peso Colombiano'},
+    {id: 'USD', nombre: 'Dolar de Estados Unidos'},
+    {id: 'MXN', nombre: 'Peso Mexicano'},
+    {id: 'EUR', nombre: 'Euro'},
+]
+
+export {
+    monedas
+}
